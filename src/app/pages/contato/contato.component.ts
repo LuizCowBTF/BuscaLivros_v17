c9ddcb265ef51a38903a8d969103c0209681733b
LuizCowBTF <luizcowbtf@gmail.com>
2024-03-06 17:13:51 -0300

    implementing hearing accessibility in the form

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-contato',
@@ -15,8 +16,9 @@ export class ContatoComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
-  ) { }
+    private router: Router,
+    private liveAnnouncer: LiveAnnouncer
+  ) {}
 
   ngOnInit() {
     this.contatoForm = this.formBuilder.group({
@@ -31,6 +33,7 @@ export class ContatoComponent {
 
   onSubmit() {
     if(this.contatoForm.valid) {
+      this.liveAnnouncer.announce('Formulário enviado com sucesso!');
       this.contatoForm.reset();
     }
   }
